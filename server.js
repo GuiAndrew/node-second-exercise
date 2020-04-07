5205d2c9bda74292bb3a39883357906b4b76ce99
GuiAndrew <guiandrew.pt@gmail.com>
2020-04-07 22:17:35 +0100

    random colors

--- server.js
+++ server.js
@@ -5,7 +5,22 @@ const arrOfColleagues = ["André", "Silvia", "Gonçalo", "João"];
 //   console.log(chalk.blue(arrOfColleague))
 // );
 
-arrOfColleagues
+// arrOfColleagues
+//   .map((arrOfColleague) => ({ sort: Math.random(), value: arrOfColleague }))
+//   .sort((x, y) => x.sort - y.sort)
+//   .map((x) => console.log(chalk.blue(x.value)));
+
+const namesRandoms = arrOfColleagues
   .map((arrOfColleague) => ({ sort: Math.random(), value: arrOfColleague }))
   .sort((x, y) => x.sort - y.sort)
-  .map((x) => console.log(chalk.blue(x.value)));
+  .map((x) => x.value);
+
+const chalkRandom = () => {
+  const colors = ["blue", "red", "yellow", "green", "cyan"];
+  const indexRandom = Math.round(Math.random() * colors.length - 1);
+  return colors[indexRandom];
+};
+
+namesRandoms.forEach((namesRandom) =>
+  console.log(chalk[chalkRandom()](namesRandom))
+);
