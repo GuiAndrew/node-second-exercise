efe1c0d4a52bc824b153780b6ddaa3605f25f3a0
GuiAndrew <guiandrew.pt@gmail.com>
2020-04-07 21:35:07 +0100

    random

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const chalk = require("chalk");
 
 const arrOfColleagues = ["André", "Silvia", "Gonçalo", "João"];
-arrOfColleagues.forEach((arrOfColleague) =>
-  console.log(chalk.blue(arrOfColleague))
-);
+// arrOfColleagues.forEach((arrOfColleague) =>
+//   console.log(chalk.blue(arrOfColleague))
+// );
+
+arrOfColleagues
+  .map((arrOfColleague) => ({ sort: Math.random(), value: arrOfColleague }))
+  .sort((x, y) => x.sort - y.sort)
+  .map((x) => console.log(chalk.blue(x.value)));
