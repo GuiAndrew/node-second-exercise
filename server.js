a599eeada5b23d00c6576b51fcdc3e354784d605
GuiAndrew <guiandrew.pt@gmail.com>
2020-04-07 20:53:49 +0100

    Done second part

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const chalk = require("chalk");
 
 const arrOfColleagues = ["André", "Silvia", "Gonçalo", "João"];
-arrOfColleagues.forEach((arrOfColleague) => console.log(chalk(arrOfColleague)));
+arrOfColleagues.forEach((arrOfColleague) =>
+  console.log(chalk.blue(arrOfColleague))
+);
